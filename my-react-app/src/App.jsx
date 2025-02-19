95d3d51f4a04222b404f4edf52b279a9e1f7a97e
Ranju Shrestha <ranjustha8844@gmail.com>
2025-02-19 11:59:42 +0545

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import Student from './Student.jsx'
 function App() {
   return(
     <>
@@ -11,7 +12,11 @@ function App() {
     <Card/>
     <Card/>
     <Button/>
-    <Footer/>
+    <Student name="Anuj" age= {23} isStudent= "true"/>
+    <Student name="Gautam" age= {23} isStudent= "false"/>
+    <Student name="Sudip" age= {20} isStudent= "true"/>
+    <Student />
+    <Footer />
     </>
   );
 
