33aff497214af5cc1e51eac37cf1d398814f53d2
Ranju Shrestha <ranjustha8844@gmail.com>
2025-02-21 22:27:55 +0545

    useState hook example

--- my-react-app/src/Button2.jsx
+++ my-react-app/src/Button2.jsx
@@ -10,25 +10,25 @@ function Buttons() {
     // return(<button onClick={() => handleClick2("Adina")}>Click me</button>) 
 
 // Another
-    // let count = 0;
+    let count = 0;
 
-    // const handleClick = (name) => {
-    //     if(count < 3) {
-    //         count ++;
-    //         console.log(`${name} you clicked me ${count} time/s`);
-    //     }
+    const handleClick = (name) => {
+        if(count < 3) {
+            count ++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
 
-    //     else {
-    //         console.log(`${name} stop clicking me`);
-    //     }
-    // };
+        else {
+            console.log(`${name} stop clicking you idoit`);
+        }
+    };
 
-    // return(<button onClick={() => handleClick("Adina")}>Click me</button>)
+    return(<button onClick={() => handleClick("Raju")}>Click me</button>)
 
 // Event Parameter
-        const handleClick = (e) => e.target.textContent ="OUCH!";
+        // const handleClick = (e) => e.target.textContent ="OUCH!";
 
-        return (<button onDoubleClick={(e) => handleClick(e)}>Click me</button>)
+        // return (<button onDoubleClick={(e) => handleClick(e)}>Click me</button>)
 
 
 }
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,17 +1,34 @@
 import React, {useState} from 'react';
 
 function MyComponent () {
-    // const [name, setName] = useState();
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed ] = useState(false)
 
-    // const updateName = () => {
-    //     setName("My name is Raju and im an idiot");
-    // }
+    const updateName = () => {
+        setName("My name is Raju and im an idiot");
+    }
 
-    // return( <div>
-    //     <p>Name: {name}</p>
-    //     <button onClick={updateName}>Set Name</button>
-    // </div>)
-    
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
+    }
+
+    return( <div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
+
+        <p>Is employed: {isEmployed ? "yes" : "No"}</p>
+        <button onClick={toggleEmployedStatus}>Toggle status</button> 
+
+    </div>)
+
 
 }
 
